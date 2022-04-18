b76147cb3199dce98bc101b828d0fd6e1b0f6044
Denys Vozniuk <vozniuk.denys96@gmail.com>
2022-04-18 20:04:43 +0100

    NEW: add getTitle test

--- src/test/java/selenium/chapters/GettingBrowserInformationTest.java
+++ src/test/java/selenium/chapters/GettingBrowserInformationTest.java
@@ -1,17 +1,24 @@
 package selenium.chapters;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import selenium.BaseTest;
 
 public class GettingBrowserInformationTest extends BaseTest {
 
-    @Test // COMING SOON
-    public void getTitleTest() {
-        /*
-        STR:
-        1) Open some URL
-        2) Assert actual title match expected
-         */
+    public String URL = "https://www.google.com/";
+
+    @BeforeMethod
+    public void openURL() {
+        driver.get(URL);
+    }
+
+    @Test
+    public void getTitle() {
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "The title was not correct!");
     }
 
     @Test // COMING SOON
