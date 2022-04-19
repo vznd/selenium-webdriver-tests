2d6a18eb3ce0f0b2bbab317a81bed5c58aff4916
Denys Vozniuk <vozniuk.denys96@gmail.com>
2022-04-19 12:17:42 +0100

    IMP: add prefix for HTML file path based on OS

--- src/main/java/selenium/HTMLPath.java
+++ src/main/java/selenium/HTMLPath.java
@@ -4,16 +4,18 @@ import java.io.File;
 
 public class HTMLPath {
 
-    public static final String FRAMES =
-            new File("src/test/resources/iframe/iframes.html").getAbsolutePath();
+    public static final String prefix = System.getProperty("os.name").contains("Windows") ? "" : "file://";
 
-    public static final String SIMPLE_ALERT =
-            new File("src/test/resources/alerts/simple-alert.html").getAbsolutePath();
+    public static final String FRAMES = prefix
+            + new File("src/test/resources/iframe/iframes.html").getAbsolutePath();
 
-    public static final String ALERT_WITH_REDIRECT =
-            new File("src/test/resources/alerts/alert-with-redirect.html").getAbsolutePath();
+    public static final String SIMPLE_ALERT = prefix
+            + new File("src/test/resources/alerts/simple-alert.html").getAbsolutePath();
 
-    public static final String PROMPT =
-            new File("src/test/resources/alerts/prompt.html").getAbsolutePath();
+    public static final String ALERT_WITH_REDIRECT = prefix
+            + new File("src/test/resources/alerts/alert-with-redirect.html").getAbsolutePath();
+
+    public static final String PROMPT = prefix
+            + new File("src/test/resources/alerts/prompt.html").getAbsolutePath();
 
 }
