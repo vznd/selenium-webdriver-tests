b6520c4cd8e4edac57c9d5d3210451a408d42303
Denys Vozniuk <vozniuk.denys96@gmail.com>
2022-05-09 18:20:39 +0100

    IMP: exclude spring boot application

--- src/main/java/vznd/selenium/HTMLPath.java
+++ src/main/java/vznd/selenium/HTMLPath.java
@@ -2,25 +2,25 @@ package vznd.selenium;
 
 public class HTMLPath {
 
-    public static final String FRAMES = "http://localhost:8080/iframes";
-    public static final String SIMPLE_ALERT = "http://localhost:8080/simple-alert";
-    public static final String ALERT_WITH_REDIRECT = "http://localhost:8080/alert-with-redirect";
-    public static final String PROMPT = "http://localhost:8080/prompt";
-    public static final String NAVIGATION = "http://localhost:8080/navigation";
-    public static final String CLICK_AND_HOLD = "http://localhost:8080/click-and-hold";
-    public static final String CONTEXT_CLICK = "http://localhost:8080/context-click";
-    public static final String DOUBLE_CLICK = "http://localhost:8080/double-click";
-    public static final String MOVE_TO_ELEMENT = "http://localhost:8080/move-to-element";;
-    public static final String KEYBOARD = "http://localhost:8080/keyboard";
-    public static final String WINDOWS = "http://localhost:8080/windows";
-    public static final String COOKIES = "http://localhost:8080/cookies";
-    public static final String FINDERS = "http://localhost:8080/finders";
-    public static final String INFORMATION = "http://localhost:8080/information";
-    public static final String INTERACTIONS = "http://localhost:8080/interactions";
-    public static final String SELECT_LISTS = "http://localhost:8080/select-lists";
-    public static final String TRADITIONAL_LOCATORS = "http://localhost:8080/traditional-locators";
-    public static final String EXPLICIT_WAIT = "http://localhost:8080/explicit-wait";
-    public static final String IMPLICIT_WAIT = "http://localhost:8080/implicit-wait";
-    public static final String FLUENT_WAIT = "http://localhost:8080/fluent-wait";
+    public static final String FRAMES = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/iframes.html";
+    public static final String SIMPLE_ALERT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/simple-alert.html";
+    public static final String ALERT_WITH_REDIRECT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/alert-with-redirect.html";
+    public static final String PROMPT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/prompt.html";
+    public static final String NAVIGATION = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/navigation.html";
+    public static final String CLICK_AND_HOLD = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/click-and-hold.html";
+    public static final String CONTEXT_CLICK = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/context-click.html";
+    public static final String DOUBLE_CLICK = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/double-click.html";
+    public static final String MOVE_TO_ELEMENT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/move-to-element.html";;
+    public static final String KEYBOARD = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/keyboard.html";
+    public static final String WINDOWS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/windows.html";
+    public static final String COOKIES = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/cookies.html";
+    public static final String FINDERS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/finders.html";
+    public static final String INFORMATION = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/information.html";
+    public static final String INTERACTIONS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/interactions.html";
+    public static final String SELECT_LISTS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/select-lists.html";
+    public static final String TRADITIONAL_LOCATORS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/traditional-locators.html";
+    public static final String EXPLICIT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/explicit-wait.html";
+    public static final String IMPLICIT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/implicit-wait.html";
+    public static final String FLUENT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/fluent-wait.html";
 
 }
--- src/main/resources/static/scripts/keyboard-scripts.js
+++
@@ -1,9 +0,0 @@
-function onEnter(event) {
-    if(event.key == "Enter") {
-        document.getElementById("green-button").style.backgroundColor = "red";
-    }
-}
-
-function setText() {
-    document.getElementById("input").text = document.getElementById("input").value;
-}
