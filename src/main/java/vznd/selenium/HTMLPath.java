3d59a800114e9ea6da5b96c324b045d031366ea4
Denys Vozniuk <vozniuk.denys96@gmail.com>
2022-10-01 20:07:30 +0100

    FIX: update html paths

--- src/main/java/vznd/selenium/HTMLPath.java
+++ src/main/java/vznd/selenium/HTMLPath.java
@@ -2,26 +2,26 @@ package vznd.selenium;
 
 public class HTMLPath {
 
-    public static final String FRAMES = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/iframes.html";
-    public static final String SIMPLE_ALERT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/simple-alert.html";
-    public static final String ALERT_WITH_REDIRECT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/alert-with-redirect.html";
-    public static final String PROMPT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/prompt.html";
-    public static final String NAVIGATION = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/navigation.html";
-    public static final String CLICK_AND_HOLD = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/click-and-hold.html";
-    public static final String CONTEXT_CLICK = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/context-click.html";
-    public static final String DOUBLE_CLICK = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/double-click.html";
-    public static final String MOVE_TO_ELEMENT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/move-to-element.html";;
-    public static final String KEYBOARD = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/keyboard.html";
-    public static final String WINDOWS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/windows.html";
-    public static final String COOKIES = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/cookies.html";
-    public static final String FINDERS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/elements-finders.html";
-    public static final String INFORMATION = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/elements-information.html";
-    public static final String INTERACTIONS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/elements-interactions.html";
-    public static final String SELECT_LISTS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/elements-select-lists.html";
-    public static final String TRADITIONAL_LOCATORS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/elements-traditional-locators.html";
-    public static final String EXPLICIT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/explicit-wait.html";
-    public static final String IMPLICIT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/implicit-wait.html";
-    public static final String FLUENT_WAIT = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/fluent-wait.html";
-    public static final String CSS_SELECTORS = "http://selenium-app.s3-website.eu-central-1.amazonaws.com/pages/css-selectors.html";
+    public static final String FRAMES = "https://vznd.github.io/selenium-app/pages/iframes.html";
+    public static final String SIMPLE_ALERT = "https://vznd.github.io/selenium-app/pages/simple-alert.html";
+    public static final String ALERT_WITH_REDIRECT = "https://vznd.github.io/selenium-app/pages/alert-with-redirect.html";
+    public static final String PROMPT = "https://vznd.github.io/selenium-app/pages/prompt.html";
+    public static final String NAVIGATION = "https://vznd.github.io/selenium-app/pages/navigation.html";
+    public static final String CLICK_AND_HOLD = "https://vznd.github.io/selenium-app/pages/click-and-hold.html";
+    public static final String CONTEXT_CLICK = "https://vznd.github.io/selenium-app/pages/context-click.html";
+    public static final String DOUBLE_CLICK = "https://vznd.github.io/selenium-app/pages/double-click.html";
+    public static final String MOVE_TO_ELEMENT = "https://vznd.github.io/selenium-app/pages/move-to-element.html";;
+    public static final String KEYBOARD = "https://vznd.github.io/selenium-app/pages/keyboard.html";
+    public static final String WINDOWS = "https://vznd.github.io/selenium-app/pages/windows.html";
+    public static final String COOKIES = "https://vznd.github.io/selenium-app/pages/cookies.html";
+    public static final String FINDERS = "https://vznd.github.io/selenium-app/pages/elements-finders.html";
+    public static final String INFORMATION = "https://vznd.github.io/selenium-app/pages/elements-information.html";
+    public static final String INTERACTIONS = "https://vznd.github.io/selenium-app/pages/elements-interactions.html";
+    public static final String SELECT_LISTS = "https://vznd.github.io/selenium-app/pages/elements-select-lists.html";
+    public static final String TRADITIONAL_LOCATORS = "https://vznd.github.io/selenium-app/pages/elements-traditional-locators.html";
+    public static final String EXPLICIT_WAIT = "https://vznd.github.io/selenium-app/pages/explicit-wait.html";
+    public static final String IMPLICIT_WAIT = "https://vznd.github.io/selenium-app/pages/implicit-wait.html";
+    public static final String FLUENT_WAIT = "https://vznd.github.io/selenium-app/pages/fluent-wait.html";
+    public static final String CSS_SELECTORS = "https://vznd.github.io/selenium-app/pages/css-selectors.html";
 
 }
